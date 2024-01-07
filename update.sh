c23542db1785f899b4433cc99ce30ad9bb023119
revstore <155325906+revstore@users.noreply.github.com>
2024-01-07 15:09:28 +0800

    update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/imam022/vip/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/revstore/scvps/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
